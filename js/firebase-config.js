2e3c9d27050fe8bb93a7eb9925b5cbe7c888bcb5
Arief Tediansyah <96020095+ZeenXxx@users.noreply.github.com>
2026-05-11 15:18:06 +0700

    admin fixing

---
+++ js/firebase-config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCPXHKoHxMUN30tJZQhWXP3yXosyLfBolU",
+  authDomain: "sipil-care.firebaseapp.com",
+  projectId: "sipil-care",
+  storageBucket: "sipil-care.firebasestorage.app",
+  messagingSenderId: "165545014090",
+  appId: "1:165545014090:web:176b1edf986827e73cc7b3",
+  measurementId: "G-XW5RX4F9VF"
+};
+
+export const app = initializeApp(firebaseConfig);
---
+++ js/js/js/admin-upload.js
@@ -0,0 +1,68 @@
+import { app } from './firebase-config.js';
+
+import {
+  getStorage,
+  ref,
+  uploadBytes,
+  getDownloadURL
+} from "https://www.gstatic.com/firebasejs/12.13.0/firebase-storage.js";
+
+import {
+  getFirestore,
+  collection,
+  addDoc
+} from "https://www.gstatic.com/firebasejs/12.13.0/firebase-firestore.js";
+
+const storage = getStorage(app);
+const db = getFirestore(app);
+
+const form = document.getElementById('uploadForm');
+
+form.addEventListener('submit', async (e) => {
+
+  e.preventDefault();
+
+  const title = document.getElementById('title').value;
+  const category = document.getElementById('category').value;
+  const description = document.getElementById('description').value;
+  const file = document.getElementById('file').files[0];
+
+  if (!file) {
+    alert('Pilih file terlebih dahulu');
+    return;
+  }
+
+  try {
+
+    const fileRef = ref(
+      storage,
+      `resources/${Date.now()}-${file.name}`
+    );
+
+    await uploadBytes(fileRef, file);
+
+    const url = await getDownloadURL(fileRef);
+
+    await addDoc(collection(db, 'resources'), {
+      title,
+      category,
+      description,
+      file: url,
+      type: 'PDF',
+      author: 'HMS Sipil',
+      date: new Date().toISOString().split('T')[0],
+      thumbnail: '📘'
+    });
+
+    alert('Upload berhasil');
+
+    form.reset();
+
+  } catch (err) {
+
+    console.error(err);
+    alert('Upload gagal');
+
+  }
+
+});
