9e757f0a972b4a33693244c03b62adb1765e1d1b
Arief Tediansyah <96020095+ZeenXxx@users.noreply.github.com>
2026-05-12 15:53:59 +0700

    fix

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCPXHKoHxMUN30tJZQhWXP3yXosyLfBolU",
-  authDomain: "sipil-care.firebaseapp.com",
-  projectId: "sipil-care",
-  storageBucket: "sipil-care.appspot.com",
-  messagingSenderId: "165545014090",
-  appId: "1:165545014090:web:176b1edf986827e73cc7b3",
-  measurementId: "G-XW5RX4F9VF"
+  apiKey: "AIzaSyDm_PmGLGQ9NEnaeMZJiphEfFFVRZhJDBk",
+  authDomain: "sipilcare.firebaseapp.com",
+  projectId: "sipilcare",
+  storageBucket: "sipilcare.firebasestorage.app",
+  messagingSenderId: "195505029208",
+  appId: "1:195505029208:web:43fab3178aee3678b2ca2d",
+  measurementId: "G-WD58LNB1G6"
 };
 
 export const app = initializeApp(firebaseConfig);
