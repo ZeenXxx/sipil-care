ebed2db2e08d40b437c66f0769b357b09d1d5edb
Arief Tediansyah <96020095+ZeenXxx@users.noreply.github.com>
2026-05-13 04:57:08 +0700

    fix login students

--- js/student-config.js
+++ js/student-config.js
@@ -1,7 +1,5 @@
 window.SIPILCARE_AUTH_CONFIG = {
-  // Ubah ke "supabase" setelah membuat project Supabase dan mengisi URL + anon key.
-  mode: "local",
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  mode: "supabase",
+  supabaseUrl: "https://bjlldukogqgrjssvssra.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJqbGxkdWtvZ3Fncmpzc3Zzc3JhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2MTE4MTAsImV4cCI6MjA5NDE4NzgxMH0.habDgFpplmwo1F_0tHvV64g2ZeICezOAZE_O5_Lbhu8",
   tableName: "students"
-};
